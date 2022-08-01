97e30781d54004d6353c38de649f8036acfd1ac1
Resh@308 <reshsingh1995@gmail.com>
2022-08-01 12:35:59 +0530

    Added:Given I am in Any Mood message Should Return HAPPY

--- src/test/java/org/example/MoodAnalyserTest.java
+++ src/test/java/org/example/MoodAnalyserTest.java
@@ -1,16 +1,13 @@
 package org.example;
 
-
-
 import org.junit.Test;
-
 import org.junit.Assert;
 
 public class MoodAnalyserTest {
     @Test
     public void TestMoodAnalysis() {
         MoodAnalyserMain moodAnalyser = new MoodAnalyserMain();
-        String mood = moodAnalyser.analyseMood("I am in sad mood");
-        Assert.assertEquals("SAD", mood);
+        String mood = moodAnalyser.analyseMood("I am in Any Mood");
+        Assert.assertEquals("HAPPY", mood);
     }
 }
