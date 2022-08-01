23555a30a09906c40e4d2a29e6558c2ab9e11f60
Resh@308 <reshsingh1995@gmail.com>
2022-08-01 10:27:55 +0530

    Added:Given I am in Sad Mood message Should Return SAD

--- src/test/java/org/example/MoodAnalyserTest.java
+++ src/test/java/org/example/MoodAnalyserTest.java
@@ -1,14 +1,16 @@
 package org.example;
 
 
+
 import org.junit.Test;
+
 import org.junit.Assert;
 
 public class MoodAnalyserTest {
     @Test
     public void TestMoodAnalysis() {
         MoodAnalyserMain moodAnalyser = new MoodAnalyserMain();
-        String mood = moodAnalyser.analyseMood("This is a happy message");
-        Assert.assertEquals("HAPPY", mood);
+        String mood = moodAnalyser.analyseMood("I am in sad mood");
+        Assert.assertEquals("SAD", mood);
     }
 }
