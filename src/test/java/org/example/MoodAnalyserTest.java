d7f6b490a8bce17538d86d3d979825027812731e
Resh@308 <reshsingh1995@gmail.com>
2022-08-02 09:48:52 +0530

    Added:Given Empty Mood Should Throw MoodAnalysisException indicating Empty Mood

--- src/test/java/org/example/MoodAnalyserTest.java
+++ src/test/java/org/example/MoodAnalyserTest.java
@@ -4,12 +4,39 @@ import org.junit.Assert;
 
 public class MoodAnalyserTest {
     @Test
-    public void TestMoodAnalysis() {
+    public void TestMoodAnalysisEnteredEmpty() {
+        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain("");
+        try {
+            moodAnalyser.analyseMood();
+        } catch (MoodAnalyserException e) {
+            Assert.assertEquals(MoodAnalyserException.ExceptionType.ENTERED_EMPTY, e.type);
+        }
+    }
+    @Test
+    public void TestMoodAnalysisEnteredNull() {
         MoodAnalyserMain moodAnalyser = new MoodAnalyserMain(null);
         try {
             moodAnalyser.analyseMood();
         } catch (MoodAnalyserException e) {
             Assert.assertEquals(MoodAnalyserException.ExceptionType.ENTERED_NULL, e.type);
         }
+    }
+    @Test
+    public void TestMoodAnalysisAnyMood() throws MoodAnalyserException{
+        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain();
+        String mood = moodAnalyser.analyseMood("I am in Any mood");
+        Assert.assertEquals("HAPPY", mood);
+    }
+    @Test
+    public void TestMoodAnalysisSadMood() throws MoodAnalyserException{
+        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain();
+        String mood = moodAnalyser.analyseMood("I am in sad mood");
+        Assert.assertEquals("SAD", mood);
+    }
+    @Test
+    public void TestMoodAnalysisHappyMood() throws MoodAnalyserException{
+        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain();
+        String mood = moodAnalyser.analyseMood("I am in happy mood");
+        Assert.assertEquals("HAPPY", mood);
     }
 }
